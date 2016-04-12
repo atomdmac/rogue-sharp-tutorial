d7f2d84f1fd9afbfebbeca41991f6a8dd896a695
suicidepills <atom.mac@gmail.com>
2016-04-12 07:59:38 -0400

    Initial commit.

---
+++ rogue-sharp-tutorial/Core/Player.cs
@@ -0,0 +1,17 @@
+﻿using roguesharptutorial.Core;
+
+namespace roguesharptutorial.Core
+{
+	public class Player : Actor
+	{
+		public Player()
+		{
+			Awareness = 15;
+			Name = "Rogue";
+			Color = Colors.Player;
+			Symbol = '@';
+			X = 10;
+			Y = 10;
+		}
+	}
+}
